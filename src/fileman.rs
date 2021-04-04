5a65a86150fbe1831fd01cc8b2a4f47bf8255f96
mWalrus <hellkvistoskar@protonmail.com>
2021-04-04 17:37:40 +0200

    fixing borrowing issue, learning a shitton, implemeted sync, install, and sync_all

--- src/fileman.rs
+++ src/fileman.rs
@@ -1,16 +1,20 @@
 extern crate toml;
 extern crate serde;
 
+#[path = "./logger.rs"]
+mod logger;
+
 use serde::Deserialize;
 use std::fs;
+use std::path::Path;
 
 #[derive(Deserialize, Clone)]
 pub struct Apps {
-    pub apps: Vec<Application>,
+    pub apps: Vec<App>,
 }
 
 #[derive(Deserialize, Clone)]
-pub struct Application {
+pub struct App {
     pub name: String,
     pub config_path: String,
     pub file_names: Vec<String>,
@@ -23,29 +27,40 @@ impl Apps {
         }
     }
 
-    pub fn find_app_by_name(&mut self, app_name: &str) -> &Application {
-        self.get_apps();
+    pub fn find_app_by_name<'a>(&'a mut self, app_name: &str) -> App {
+        self.read_apps();
         let pos: usize = self.apps.iter().position(|i| i.name == app_name).unwrap();
-        self.apps.get(pos).unwrap()
+        let app = self.apps.get(pos).unwrap();
+        app.clone()
     }
 
-    pub fn get_apps(&mut self) {
+    pub fn read_apps(&mut self) {
         let file_content: String = fs::read_to_string("Applications.toml").unwrap();
         self.apps = toml::from_str::<Apps>(&file_content).unwrap().apps;
     }
+}
 
-    pub fn copy_files(&mut self, app: &Application, source: &str, dest: &str) -> std::io::Result<()> {
-        for file in &app.file_names {
-            std::fs::copy(source.to_string() + file, dest)?;
-        }
-        Ok(())
+pub fn copy_files(file_names: Vec<String>, source: &str, dest: &str) -> std::io::Result<()> {
+    if !Path::new(source).exists() {
+        fs::create_dir(source).unwrap(); // create source dir if nonexistant
     }
+    if !Path::new(dest).exists() {
+        fs::create_dir(dest).unwrap(); // create destination dir if nonexistant
+    }
+    for file in file_names {
+        let source_path = source.to_string() + &file;
+        let dest_path = dest.to_string() + &file;
+        // check if source file exists before attempting copy
+        assert!(Path::new(&source_path).exists());
+        fs::copy(source_path, dest_path)?;
+    }
+    Ok(())
+}
 
-    pub fn remove_files(&self, path: &str) {
-        let files = fs::read_dir(path);
-        for file in files {
-            println!("{:?}", file);
-        }
+pub fn remove_files(path: &str) {
+    let files = fs::read_dir(path);
+    for file in files {
+        println!("{:?}", file);
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ mod args;
 mod logger;
 
 use colored::*;
-use fileman::{Application, Apps};
+use fileman::{Apps};
 
 //hej jag heter ellen. jag älskar dig även fast du tycker jag är jobbig. glad smiley
 
@@ -23,8 +23,18 @@ fn take_new_application() {
     println!("App name: {}\nPath: {}\nFiles: {}", app_name, app_config_path, config_files);
 }
 
+// Gets the absolute path to a config directory
 fn get_absolute_path(relative_path: &str) -> String {
     home::home_dir().unwrap().display().to_string() + "/" + relative_path + "/"
+}
+
+// Prints general information about an app
+fn print_app_info(conf_path: &str, file_names: Vec<String>) {
+    logger::print_info("Found app config path: ".to_owned() + conf_path);
+    logger::print_info("Found files to handle:".to_owned());
+    for name in file_names.iter() {
+        println!("    {} {}", "=>".bold(), name);
+    }
 }
 
 fn sync_application(app_name: &str) {
@@ -33,28 +43,30 @@ fn sync_application(app_name: &str) {
     let app = apps.find_app_by_name(&app_name);
     let conf_path = get_absolute_path(&app.config_path);
     let rel_path = "./".to_string() + &app.name + "/";
-    apps.copy_files(&app, &conf_path, &rel_path);
+    print_app_info(&conf_path, app.clone().file_names);
+    fileman::copy_files(app.file_names, &conf_path, &rel_path).unwrap();
 }
 
 fn sync_all_applications() {
-    println!("sync all applications");
+    logger::print_job("Syncing all applications".to_owned());
     let mut apps = Apps::new();
-    apps.get_apps();
-    let app_list = &mut apps.apps;
-    for app in app_list {
+    apps.read_apps();
+    let app_list = apps.apps;
+    for app in app_list.iter() {
         let conf_path = get_absolute_path(&app.config_path);
         let rel_path = "./".to_string() + &app.name + "/";
-        apps.copy_files(app, &conf_path, &rel_path);
+        print_app_info(&conf_path, app.clone().file_names);
+        fileman::copy_files(app.clone().file_names, &conf_path, &rel_path).unwrap();
     }
 }
 
 fn install_application(app_name: &str) {
-    println!("install application");
+    logger::print_job("Installing application".to_owned() + &app_name);
     let mut apps = fileman::Apps::new();
     let app = apps.find_app_by_name(&app_name);
     let conf_path = get_absolute_path(&app.config_path);
     let rel_path = "./".to_string() + &app.name + "/";
-    apps.copy_files(&app, &conf_path, &rel_path);
+    fileman::copy_files(app.file_names, &conf_path, &rel_path).unwrap();
 
 }
 
